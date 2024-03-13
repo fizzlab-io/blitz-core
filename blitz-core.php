d2cc791b3f06f9f692e80d28f049d1afa3170117
Sebastian Inman <sebastian@fizzlab.io>
2024-03-13 01:53:02 -0700

    first commit

---
+++ blitz-core.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Blitz Core
+ * Plugin URI: https://fizzlab.io/wordpress/plugins/blitz-core
+ * Description: This plugin is used to add core functionality to the theme.
+ * Version: 1.0
+ * Author: FizzLab
+ * Author URI: https://fizzlab.io
+ */
+
+if (!defined("ABSPATH")) {
+    exit;
+}
+
+class Bltiz {
+
+    public function __construct() {
+        
+    }
+
+}
