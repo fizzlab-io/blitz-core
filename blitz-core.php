43f8058f2a205004c4e4fb662bf53383ce3f4fad
Sebastian Inman <sebastian@fizzlab.io>
2024-03-13 02:26:43 -0700

    [test] testing plugin class structure

--- blitz-core.php
+++ blitz-core.php
@@ -9,12 +9,48 @@
  * Author URI: https://fizzlab.io
  */
 
-namespace FizzLab\Blitz\Core;
+if (!class_exists("Blitz_Plugin_Core")) {
 
-if (!defined("ABSPATH")) {
-    exit;
+    class Blitz_Plugin_Core {
+
+        public static $instance;
+
+        public function __construct() {
+
+            // When the rest of WP has loaded, kick-start the rest of the class.
+			add_action("init", array($this, "init"));
+
+        }
+
+        /**
+		 * Magic method to (not) set protected properties from outside of this class.
+		 *
+		 * @param string $name  Name of an inaccessible property.
+		 * @param mixed  $value Value to assign to the property.
+		 * @return void  Silently fail to set the property when this is tried from outside of this class context.
+		 *               (Inside this class context, the __set() method if not used as there is direct access.)
+		 */
+		public function __set($name, $value) {
+			return;
+		}
+
+        /**
+		 * Magic method to get the value of a protected property outside of this class context.
+		 *
+		 * @param string $name Name of an inaccessible property.
+		 * @return mixed The property value.
+		 */
+		public function __get($name) {
+			return $this->{$name};
+		}
+
+        /**
+         * Initialize the interactions between this class and WordPres.
+         */
+        public function init() {
+            echo "Hello from the Blitz_Plugin_Core class `init` method.";
+        }
+
+    }
+
 }
-
-use function FizzLab\namespace_test;
-
-namespace_test();
